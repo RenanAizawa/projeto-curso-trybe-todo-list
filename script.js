cc4b847e525d6dc83c897ab940c541ffc4ee3f23
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2021-12-29 19:57:18 -0500

    Q. 8, resolvida

--- script.js
+++ script.js
@@ -9,7 +9,14 @@ buttonTask.addEventListener('click', function(){
     itemList.innerText = inputFiled.value;
     lisTask.appendChild(itemList)
     inputFiled.value = "";
-    itemList.addEventListener('click', function(){
-        itemList.style.backgroundColor = `rgb(128, 128, 128)`
+    itemList.addEventListener('click', function(event){
+        let itensLi = document.querySelectorAll('li');
+        for (var i = 0; i < itensLi.length; i++) {
+            var selectLi = document.querySelectorAll('li')[i];
+            selectLi.classList.remove('selected-item')
+        }
+        var alvo = event.target;
+        alvo.classList.add('selected-item') 
+        // itemList.style.backgroundColor = `rgb(128, 128, 128)`
     })
 })
